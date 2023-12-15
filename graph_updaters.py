781e38236ec0189937a3864569fbab0849481181
CHB-0r1s <borisluzin2004@gmail.com>
2023-12-16 00:11:44 +0300

    Reformat code for linting purpose

--- graph_updaters.py
+++ graph_updaters.py
@@ -2,7 +2,9 @@ from typing import Iterable
 from matplotlib.lines import Line2D
 
 
-def simple_update(frame: int, x_list: list, y_list: list, line: Line2D) -> Iterable[Line2D]:
+def simple_update(
+    frame: int, x_list: list, y_list: list, line: Line2D
+) -> Iterable[Line2D]:
     X = x_list[:frame]
     Y = y_list[:frame]
     line.set_xdata(X)
